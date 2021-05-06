f2a52284c1e382768f7c30ceabd67d709520ee6b
Adarsh Kumar Mishra (iCORE-CIS) <AD20018359@wipro.com>
2021-05-06 17:49:58 +0530

    test1

--- src/authConfig.js
+++ src/authConfig.js
@@ -60,5 +60,5 @@ export const loginRequest = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const graphConfig = {
-    graphMeEndpoint: "https://graph.microsoft.com"
+    graphMeEndpoint: "https://localhost:3000/api"
 };
